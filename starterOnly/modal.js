757d2a0dfd64fa9544b715644d582d7babc15265
portanoo <portanoo@gmail.com>
2023-02-22 00:49:51 +0100

    Issue #1 - Close the Modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,9 +15,17 @@ const formData = document.querySelectorAll(".formData");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+modalbg.querySelector(".close").addEventListener("click", closeModal);
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
